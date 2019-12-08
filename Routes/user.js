3809cd9d81d60bed7c8b760275e89ff23b804d1b
HamidRazaShaikh <hamidraza.chemistry@gamil.com>
2019-12-08 05:30:03 -0800

    3rd commit'

--- Routes/user.js
+++ Routes/user.js
@@ -8,16 +8,16 @@ user.post('/add',function (req,res) {
     var addData = new userModel (req.body);
     addData.save(function (error,data) {
         res.send({error : error , data : data});
-        console.log(error);
         console.log(data);
+        console.log(error);
     })
 });
 
 user.get('/get' , function (req,res) {
     userModel.find().exec(function (error,data) {
         res.send({error : error , data : data});
-        console.log(error);
-        console.log(data)
+        console.log(data);
+        console.log(error)
     })
 });
 
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var user = require('./Routes/user');
 var app = express();
-var schema = require('./Routes/schema');
-var userModel = schema.userModel;
+
+
 
 var uri = "mongodb+srv://hamid:hamid@hamid-yq7y8.mongodb.net/Studentdata?retryWrites=true&w=majority";
 mongoose.connect(uri, {useNewUrlParser : true});
@@ -14,7 +14,7 @@ mongoose.connect(uri, {useNewUrlParser : true});
 app.use(bodyParser.json({limit: '5000kb'}));
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(cors());
-app.use('/user', user);
+app.use('/Studentdata', user);
 
 
 app.set('port' , process.env.PORT || 4000);
